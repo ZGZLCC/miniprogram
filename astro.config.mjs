bbed9ff739fc8cebb054173000ad4b26e1aab2e0
ZGZLCC <1075300572@qq.com>
2026-01-12 13:50:38 +0800

    fix(config): 更新站点URL配置
    将站点根URL从 https://zgzlcc.github.io/miniprogram/
    更改为 https://miniprogram.zgzlcc.top

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://zgzlcc.github.io/miniprogram/', // Root URL of site
+  site: 'https://miniprogram.zgzlcc.top', // Root URL of site
   i18n: {
     locales: ['zh-cn', 'en'],
     defaultLocale: 'zh-cn',
